489cb72408c1b6fbb4c6bb562e1a50c423d3686e
Oleksiy Pastukhov <oleksiy.pastukhov@senseteq.io>
2021-02-26 16:05:56 +0200

    Added exports to the index.js file

---
+++ src/index.js
@@ -0,0 +1,103 @@
+import Alert from './components/Alert';
+import Avatar from './components/Avatar';
+import Badge from './components/Badge';
+import Button from './components/Button';
+import ButtonItem from './components/ButtonItem';
+import Card from './components/Card';
+import Checkbox from './components/Checkbox';
+import Collapse from './components/Collapse';
+import Box from './components/Containers/Box';
+import Row from './components/Containers/Row';
+import Col from './components/Containers/Col';
+import DatePicker from './components/DatePicker';
+import Divider from './components/Divider';
+import Icon from './components/Icon';
+import Image from './components/Image';
+import InfoText from './components/InfoText';
+import Input from './components/Input';
+import Item from './components/Item';
+import Link from './components/Link';
+import Menu from './components/Menu';
+import MenuItem from './components/MenuItem';
+import Spinner from './components/Spinner';
+import Status from './components/Status';
+import Switch from './components/Switch';
+import Text from './components/Text';
+import Toggle from './components/Toggle';
+import Upload from './components/Upload';
+import AutoScroll from './components/AutoScroll';
+import useTheme from './design-system/context/useTheme';
+import ThemeProvider from './design-system/context/Theme.provider';
+
+export {
+  Alert,
+  Avatar,
+  Badge,
+  Button,
+  ButtonItem,
+  Card,
+  Checkbox,
+  Collapse,
+  Box,
+  Row,
+  Col,
+  DatePicker,
+  Divider,
+  Icon,
+  Image,
+  InfoText,
+  Input,
+  Item,
+  Link,
+  Menu,
+  MenuItem,
+  Spinner,
+  Status,
+  Switch,
+  Text,
+  Toggle,
+  Upload,
+  AutoScroll,
+  useTheme,
+  ThemeProvider,
+};
+
+export default {
+  Alert,
+  Avatar,
+  Badge,
+  Button,
+  ButtonItem,
+  Card,
+  Checkbox,
+  Collapse,
+  Box,
+  Row,
+  Col,
+  DatePicker,
+  Divider,
+  Icon,
+  Image,
+  InfoText,
+  Input,
+  Item,
+  Link,
+  Menu,
+  MenuItem,
+  PopConfirm,
+  Radio,
+  Rating,
+  SectionList,
+  Select,
+  Skeleton,
+  Spinner,
+  Statistic,
+  Status,
+  Switch,
+  Text,
+  Toggle,
+  Upload,
+  AutoScroll,
+  useTheme,
+  ThemeProvider,
+};
