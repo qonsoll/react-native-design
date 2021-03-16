0e359969bb922d70a85526a599ff234ad75582f1
Evgeniy Bogdanov <evgeniybogdanov@MacBook-Pro-Evgeniy.local>
2021-03-16 20:49:54 +0200

    Updates for Text v4

--- src/design-system/hooks/useFontVariant.js
+++ src/design-system/hooks/useFontVariant.js
@@ -2,13 +2,11 @@ import {useTheme} from '../context';
 import {fontVariants} from '../helpers';
 import {StyleSheet} from 'react-native';
 
-const DEFAULT_FONT_VARIANT = 'body2'
-
 const useFontVariant = (value) => {
   const {theme} = useTheme();
   const FONT_VARIANTS = fontVariants({theme});
   const style = StyleSheet.create({
-    main: FONT_VARIANTS[value] || DEFAULT_FONT_VARIANT,
+    main: FONT_VARIANTS[value] || FONT_VARIANTS['body2']
   });
   return style.main;
 };
