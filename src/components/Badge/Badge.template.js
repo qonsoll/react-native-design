4c0868f8ebaa3ca9296f413ec94428c8e9c14166
Evgeniy Bogdanov <evgeniybogdanov@MacBook-Pro-Evgeniy.local>
2021-03-18 17:40:13 +0200

    Badge: changed proptype fot prop text

--- src/components/Badge/Badge.template.js
+++ src/components/Badge/Badge.template.js
@@ -15,13 +15,13 @@ const Badge = (props) => {
   const {BADGES} = theme.components;
 
   // Template
-  return <Item text={text} size={size} {...BADGES.variants[variant || DEFAULT_VARIANT]} />;
+  return <Item text={text} {...BADGES.variants[variant || DEFAULT_VARIANT]} size={size} />;
 };
 
 // PropTypes
 Badge.propTypes = {
   variant: PropTypes.string,
-  text: PropTypes.string,
+  text: PropTypes.any,
   size: PropTypes.string,
 }
 
